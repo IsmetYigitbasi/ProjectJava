ae1050173fe921ed17adb9bc0e405f7db66f9752
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-13 18:32:39 +0200

    Added example of printing

--- src/test/java/PrintExample.java
+++ src/test/java/PrintExample.java
@@ -1,11 +1,13 @@
 public class PrintExample {
 
     /*
-            We need type System class name then
+            We need type System class to print something.
+            In this example the string data type is being printed
+
      */
 
     public static void main(String[] args){
-
+        // here is a main method that JVM starts
         System.out.println("This is printing example");
     }
 }
