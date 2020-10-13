1d9fb8c208407bd7f0082165cfbf1372c1937e8c
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-13 18:28:06 +0200

    Added example of printing

---
+++ src/test/java/PrintExample.java
@@ -0,0 +1,11 @@
+public class PrintExample {
+
+    /*
+            We need type System class name then
+     */
+
+    public static void main(String[] args){
+
+        System.out.println("This is printing example");
+    }
+}
